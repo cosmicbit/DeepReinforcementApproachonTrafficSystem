293cd2e99ee962007c40e890f9e53644f9ba8a06
cosmicbit <philipsjose2003@gmail.com>
2024-12-18 15:56:02 +0530

    virtual env files

--- routesgeneration.py
+++ routesgeneration.py
@@ -71,7 +71,7 @@ def generate_random_routes(num_vehicles, num_routes):
                 if next_nodes in node_route:
                     break
                 else:
-                    current_node = random.choice(next_nodes except current_node)
+                    current_node = random.choice(list(filter(lambda x: x != current_node, next_nodes)))
             node_route.append(current_node)
         print("--------------------------node route=",node_route)
         edge_route = [edges[(node_route[j], node_route[j + 1])] for j in range(len(node_route) - 1)]
